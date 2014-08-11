8752c1f4c2405a755c160908af22c2b60c9cdb73
Jon Black <Jon.Black@radboudumc.nl>
2014-08-11 10:47:34 +0200

    Add url parameter to example (#2)

--- examples/pdb_to_hssp.py
+++ examples/pdb_to_hssp.py
@@ -1,6 +1,10 @@
 """
 This example client takes a PDB file, sends it to the REST service which
 converts it to a HSSP file. The HSSP content is then output to the console.
+
+Example:
+
+    python pdb_to_hssp 1crn.pdb http://localhost:5000/
 """
 
 import argparse
@@ -9,14 +13,11 @@ import requests
 import time
 
 
-XSSP_REST_URL_BASE = 'http://127.0.0.1:5000/'
-
-
-def pdb_to_hssp(pdb_file_path):
+def pdb_to_hssp(pdb_file_path, rest_service_url):
     with open(pdb_file_path) as pdb_file:
         pdb_content = pdb_file.read()
 
-    r = requests.post(XSSP_REST_URL_BASE + 'api/create/hssp/from_pdb/',
+    r = requests.post(rest_service_url + 'api/create/hssp/from_pdb/',
                       data={'pdb_content': pdb_content})
     r.raise_for_status()
     job_id = json.loads(r.text)['id']
@@ -24,7 +25,7 @@ def pdb_to_hssp(pdb_file_path):
 
     ready = False
     while not ready:
-        r = requests.get(XSSP_REST_URL_BASE +
+        r = requests.get(rest_service_url +
                          'api/job/hssp_from_pdb/{}/status/'.format(job_id))
         r.raise_for_status()
 
@@ -35,7 +36,7 @@ def pdb_to_hssp(pdb_file_path):
         else:
             time.sleep(5)
     else:
-        r = requests.get(XSSP_REST_URL_BASE +
+        r = requests.get(rest_service_url +
                          'api/job/hssp_from_pdb/{}/result/'.format(job_id))
         r.raise_for_status()
         result = json.loads(r.text)['result']
@@ -46,8 +47,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Convert a PDB file to a HSSP file')
     parser.add_argument('pdb_file_path')
-
+    parser.add_argument('rest_service_url')
     args = parser.parse_args()
 
-    result = pdb_to_hssp(args.pdb_file_path)
+    result = pdb_to_hssp(args.pdb_file_path, args.rest_service_url)
     print result
