3b76e6e3ec2ba393f5f4e9886dfc5ca6919bda5e
Coos Baakman <coos.baakman@radboudumc.nl>
2022-01-10 12:03:45 +0000

    should be flaskform

--- xssp_api/frontend/dashboard/forms.py
+++ xssp_api/frontend/dashboard/forms.py
@@ -1,8 +1,8 @@
 import re
 
 from xssp_api import default_settings as settings
-from flask_wtf import Form
-from wtforms.fields import FileField, SelectField, TextAreaField, StringField
+from flask_wtf import FlaskForm
+from wtforms.fields import FileField, SelectField, TextAreaField, StringField, HiddenField
 from wtforms.validators import Regexp
 
 from xssp_api.frontend.validators import (FileExtension, NAminoAcids,
@@ -12,7 +12,8 @@ from xssp_api.frontend.validators import (FileExtension, NAminoAcids,
 RE_PDB_ID = re.compile(r"^[0-9a-zA-Z]{4}$")
 
 
-class XsspForm(Form):
+class XsspForm(FlaskForm):
+
     input_type = SelectField(u'Input',
                              choices=[('pdb_id', 'PDB code'),
                                       ('pdb_redo_id', 'PDB_REDO code'),
